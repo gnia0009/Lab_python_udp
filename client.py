788a73ee556819c3c096548440209a1e9ede5018
Aby gning <gnia0009@uqar.ca>
2026-02-12 10:29:45 -0500

    Partie 4hashlib

--- client.py
+++ client.py
@@ -20,16 +20,25 @@ print(type(s2))
 
 
 
+
+
 import socket
+import hashlib
 from pathlib import Path
 
 HOST = "127.0.0.1"
-PORT = 12345
+PORT = 12346
 
+#Ici c'est pour lire message
 msg = Path("data/message.txt").read_text(encoding="utf-8").encode("utf-8")
 
+h = hashlib.sha256(msg).hexdigest()
+
+# Créer payload : message + séparateur + hash
+payload = msg + b"\x00" + h.encode("ascii")
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.sendto(msg, (HOST, PORT))
+    s.sendto(payload, (HOST, PORT))
 
     data, _ = s.recvfrom(1024)
-    print("Réponse du serveur :", data.decode("utf-8"))
+    print("Réponse serveur :", data.decode("utf-8"))
--- server.py
+++ server.py
@@ -1,16 +1,31 @@
+
+
 import socket
+import hashlib
 
 HOST = "127.0.0.1"
-PORT = 12345
+PORT = 12346
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
-    print(f"Serveur UDP en écoute sur {HOST}:{PORT}")
+    print(f"Serveur UDP sur {HOST}:{PORT}")
 
     while True:
-        data, addr = s.recvfrom(1024)
+        data, addr = s.recvfrom(2048)
 
-        print(f"Reçu {len(data)} octets de {addr}")
-        print("Message :", data.decode("utf-8", errors="replace"))
+        try:
+            # Séparer message et hash
+            message, hash_hex = data.split(b"\x00", 1)
 
-        s.sendto(b"Message recu", addr)
+            # Recalculer hash
+            calc = hashlib.sha256(message).hexdigest().encode("ascii")
+
+            if calc == hash_hex:
+                print("Message valide reçu")
+                s.sendto(b"Message et hachage valides", addr)
+            else:
+                print("Hash invalide")
+                s.sendto(b"Erreur de hachage", addr)
+
+        except Exception:
+            s.sendto(b"Format invalide", addr)
